dd870ae7077bf702e88ef4620c7e55f10e40c959
codecaiine <akabrouyannickn@gmail.com>
2021-12-14 08:05:12 +0000

    Change users view title

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
