7a145c460453311aedb321ec8bc7ee18decfdaee
codecaiine <akabrouyannickn@gmail.com>
2021-12-14 20:16:24 +0000

    Add tests for posts show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,19 +1,31 @@
 require 'rails_helper'
-
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
     before(:example) { get user_posts_path(13) }
+    it 'checks if action returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'checks if action rendered a correct template' do
+      expect(response).to render_template('index')
+    end
+    it 'checks if correct placeholder is shown' do
+      expect(response.body).to include('List all posts')
+    end
+  end
+
+  describe "GET /show" do
+    before(:example) { get user_post_path(13, 45) }
 
     it 'checks if action returns correct response status' do
       expect(response).to have_http_status(:ok)
     end
 
     it 'checks if action rendered a correct template' do
-      expect(response).to render_template('index')
+      expect(response).to render_template('show')
     end
 
     it 'checks if correct placeholder is shown' do
-      expect(response.body).to include('List all posts')
+      expect(response.body).to include('Show single post')
     end
   end
 end
