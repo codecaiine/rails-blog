abed1ad820184654e5effd19475dde5fe8298dde
codecaiine <akabrouyannickn@gmail.com>
2021-12-14 08:11:15 +0000

    Nest Posts Route Within Users Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :users do
+    resources :posts
+  end
 end
