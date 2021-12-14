1aabedc1459109e6a73b3ed10e6eb068378b7156
codecaiine <akabrouyannickn@gmail.com>
2021-12-14 20:13:01 +0000

    Add tests for posts index action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get user_posts_path(13) }
+
+    it 'checks if action returns correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'checks if action rendered a correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'checks if correct placeholder is shown' do
+      expect(response.body).to include('List all posts')
+    end
   end
 end
