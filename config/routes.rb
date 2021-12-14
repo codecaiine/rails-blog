77fb18e7ef839c305ca632df0a15762a17afd07f
codecaiine <akabrouyannickn@gmail.com>
2021-12-14 08:56:49 +0000

    Add routes requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
+    resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 end
