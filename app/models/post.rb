99dd4d794a13003c0964e5ddecba023c71237c79
codecaiine <akabrouyannickn@gmail.com>
2021-12-20 20:48:51 +0000

    Create a post update methods for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
-
     has_many :comments
     has_many :likes
-    belongs_to :users
+  
+    belongs_to :author, class_name: 'User'
+  
+    def update_post_counter
+      author.increment!(:posts_counter)
+    end
 end
