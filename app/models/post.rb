6bdd144d0d1a77de87ab8870f13e5605fa5c7dd5
codecaiine <akabrouyannickn@gmail.com>
2021-12-20 20:52:25 +0000

    Create a methods a recent comment in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
     def update_post_counter
       author.increment!(:posts_counter)
     end
+
+    def recent_comments
+        comments.limit(5).order(created_at: :desc)
+    end
 end
