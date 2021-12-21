a8d390c81a6f58a3bf9197d6556349723a4f4e3f
codecaiine <akabrouyannickn@gmail.com>
2021-12-21 23:56:36 +0000

    Add style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_134145) do
+ActiveRecord::Schema.define(version: 2021_12_15_020913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,18 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_134145) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "likes", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_likes_on_post_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_12_15_134145) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
