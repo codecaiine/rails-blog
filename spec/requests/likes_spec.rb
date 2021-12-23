4007f6c6ec34230b654afc8450860194445aa084
codecaiine <akabrouyannickn@gmail.com>
2021-12-23 15:27:00 +0000

    Modify like spec file

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Likes', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/likes/create'
+      expect(response).to have_http_status(:success)
+    end
   end
 end
