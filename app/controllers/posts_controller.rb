6415255b7d378ecd2acecdd9cf37fc7f811960a9
codecaiine <akabrouyannickn@gmail.com>
2021-12-23 21:35:28 +0000

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
   end
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
