b9170ecb98da9bf8fb2ec25eebf885f7b8fc5725
codecaiine <akabrouyannickn@gmail.com>
2021-12-24 15:58:59 +0000

    Fix likes counter to increment

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :update_likes_counter
+
   def update_likes_counter
-    post.increment!(:likes_counter)
+    post.update(likes_counter: post.likes.size)
   end
 end
