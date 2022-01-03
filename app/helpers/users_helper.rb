c7e312bb58a7d1d511a6d6af2af2c4189f87c1e5
codecaiine <akabrouyannickn@gmail.com>
2022-01-03 22:02:52 +0000

    Add user picture

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def user_img(user)
-    user.photo.present? ? user.photo : '../assets/images/user.png'
+    "https://avatars.githubusercontent.com/u/6615394?s=400&u=726d3c804c97e6fa8d2b540d5ff0b93940eaf626&v=4"
   end
 end
