acb2911446f75eacf7bf5ba145dcaf51ee254a29
codecaiine <akabrouyannickn@gmail.com>
2022-01-05 22:31:59 +0000

    Remove unnecessary files and folder

--- app/helpers/users_helper.rb
+++
@@ -1,5 +0,0 @@
-module UsersHelper
-  def user_img(user)
-    user.photo.present? ? user.photo : 'https://avatars.githubusercontent.com/u/6615394?v=4'
-  end
-end
--- db/migrate/20220105120823_add_post_ref_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddPostRefToLikes < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :likes, :author, foreign_key: { to_table: :users }
-  end
-end
