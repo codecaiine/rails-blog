eaa72b0743c18ec355a4acbdd4e0c466d691c89c
codecaiine <akabrouyannickn@gmail.com>
2022-01-06 22:14:14 +0000

    Add api endpoint for posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::PostsController < ApplicationController
+    load_and_authorize_resource
+  
+    def index
+      @posts = Post.all
+      render json: @posts, status: 200
+    end
+end
