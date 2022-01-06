aba2a1efb3aa45b329c210755bbd0d4d0e70326f
codecaiine <akabrouyannickn@gmail.com>
2022-01-06 22:17:24 +0000

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
