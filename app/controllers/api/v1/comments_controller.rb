2269acb301f2e2cca25f44543c5cfc022b66da52
codecaiine <akabrouyannickn@gmail.com>
2022-01-07 00:34:59 +0000

    Add api routes

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,27 +1,26 @@
 class Api::V1::CommentsController < ApplicationController
-    load_and_authorize_resource
-  
-    def index
-      @post = Post.find(params[:post_id])
-      @comments = @post.comments.all
-      render json: @comments
+  load_and_authorize_resource
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
+    render json: @comments
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(text: comment_parameters[:text], author_id: current_user.id)
+
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors, status: :bad_request, message: 'Failed to add comment'
     end
-  
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(text: comment_parameters[:text], author_id: current_user.id)
-  
-      if @comment.save
-        render json: @comment
-      else
-        render json: @comment.errors, status: :bad_request, message: 'Failed to add comment'
-      end
-  
-    end
-  
-    private
-  
-    def comment_parameters
-      params.require(:comment).permit(:text)
-    end
-  end 
+  end
+
+  private
+
+  def comment_parameters
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::ApiController
+  load_and_authorize_resource
+
   def index
     @users = User.all
+    render json: @users, status: 200
   end
 
   def show
     @user = User.find(params[:id])
+    render json: { success: true, data: { user: @user, message: 'user retrieved succeffully' } }
   end
 end
