fcf04d23e4249e2c639350e714ea8cba50b0ac34
codecaiine <akabrouyannickn@gmail.com>
2022-01-07 08:51:53 +0000

    Remove corrupt files and folder

--- app/controllers/api/v1/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::PostsController < ApplicationController
-  load_and_authorize_resource
-
-  def index
-    @posts = Post.all
-    render json: @posts, status: 200
-  end
-end
--- db/migrate/20220107000132_create_jwt_denylist.rb
+++
@@ -1,9 +0,0 @@
-class CreateJwtDenylist < ActiveRecord::Migration[6.1]
-  def change
-    create_table :jwt_denylists do |t|
-      t.string "jti", null: false
-      t.datetime "exp", null: false
-      t.index ["jti"], name: "index_jwt_denylists_on_jti"
-    end
-  end
-end
